df9f72bf9460f2c7a4144496b47edf9b6342ca4f
Maneesha Dubey <maneeshadubey108@gmail.com>
2023-08-10 17:17:02 +0530

    changes done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,42 +1,53 @@
+import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
+
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=Mumbai,New Delhi,Hyderabad"
+    );
   return (
     <div className="featured">
+       {loading ? (
+        "Loading please wait"
+      ) : (
+        <>
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          src="https://img.freepik.com/free-photo/mumbai-skyline-seen-from-marine-drive-south-mumbai_469504-11.jpg?w=1380&t=st=1690918844~exp=1690919444~hmac=93aadbd57216fc774e05462f1024d1476a8d269e26fa213fe21e5c5cd57348e4"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Dublin</h1>
-          <h2>123 properties</h2>
+          <h1>Mumbai</h1>
+          <h2>1 properties</h2>
         </div>
       </div>
       
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+          src="https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20181121185425/Charminar-Hyderabad.jpg"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Reno</h1>
-          <h2>533 properties</h2>
+          <h1>Hyderabad</h1>
+          <h2>2 properties</h2>
         </div>
       </div>
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+          src="https://www.revv.co.in/blogs/wp-content/uploads/2019/11/self-drive-car-rental-delhi-gurgaon-noida_revv-blog.jpg"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Austin</h1>
-          <h2>532 properties</h2>
+          <h1>Delhi</h1>
+          <h2>1 properties</h2>
         </div>
       </div>
+      </>
+     )}
     </div>
   );
 };
